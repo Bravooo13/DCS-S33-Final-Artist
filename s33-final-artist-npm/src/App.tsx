a3c5548f21164e4b31cd32dfd6750bb936578fec
liettexx <enkhoyuka469@gmail.com>
2024-05-21 10:39:49 -0400

    Updating App.tsx

--- s33-final-artist-npm/src/App.tsx
+++ s33-final-artist-npm/src/App.tsx
@@ -1,46 +1,25 @@
-<<<<<<< HEAD
 import { useState } from "react";
-import "./App.css";
+import "./style.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-=======
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavBar from "./components/Navbar";
-import HomePage from "./components/HomePage";
->>>>>>> 78bc631b0d962a63cc9635e45c528dc7033f5841
+  import React from "react";
+  import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+  import NavBar from "./components/Navbar";
+  import HomePage from "./components/HomePage";
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <div className="bg-black min-h-screen">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          {/* Add other routes for Art and Poetry here */}
-        </Routes>
-      </div>
-<<<<<<< HEAD
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  const App: React.FC = () => {
+    return (
+      <Router>
+        <div className="bg-black min-h-screen">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* Add other routes for Art and Poetry here */}
+          </Routes>
+        </div>
+      </Router>
+    );
+  };
 }
-=======
-    </Router>
-  );
-};
->>>>>>> 78bc631b0d962a63cc9635e45c528dc7033f5841
 
 export default App;
