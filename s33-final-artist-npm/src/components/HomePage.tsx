95487f6eda7d8444bd6a88a7e3292fd541cae510
Elvis Zhou <elvis_zhou@outlook.com>
2024-05-22 11:34:24 -0400

    1

--- s33-final-artist-npm/src/components/HomePage.tsx
+++ s33-final-artist-npm/src/components/HomePage.tsx
@@ -4,7 +4,7 @@ import { GalleryWithCarousel } from "./Gallary";
 
 const HomePage: React.FC = () => {
   return (
-    <div className="relative flex items-center justify-center min-h-screen bg-black">
+    <div className="relative flex items-center justify-center min-h-screen bg-white">
       <NavBar />
       <GalleryWithCarousel />
       <h1 className="absolute text-9xl text-white">Art Gallery</h1>
